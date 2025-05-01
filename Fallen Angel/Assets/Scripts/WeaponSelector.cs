3cff0e57975795dde2b7e63dcf61916928bd6e1d
abhiphc <abhijitranjan15@gmail.com>
2025-05-01 22:11:39 +0530

    Gun selector some work completed

--- Fallen Angel/Assets/Scripts/WeaponSelector.cs
+++ Fallen Angel/Assets/Scripts/WeaponSelector.cs
@@ -1,11 +1,19 @@
+using NUnit.Framework;
 using UnityEngine;
-
+using System.Collections;
+using System.Collections.Generic;
 public class WeaponSelector : MonoBehaviour
 {
     [SerializeField] bool isSelected = false;
+    public int selectedWeaponIndex;
+    [SerializeField] List<Transform> weapons = new List<Transform>();
+    void Start()
+    {
+        selectedWeaponIndex = 0;
+    }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1)) // Gun mode can activate or deactivate through this key
+        if (Input.GetKeyDown(KeyCode.G)) // Gun mode can activate or deactivate through this key
         {
             if(!Input.GetMouseButton(1)) // While pressing RMB the gun mode cannot be activated
             {
@@ -22,10 +30,43 @@ public class WeaponSelector : MonoBehaviour
                 if (isSelected)
                 {
                     PlayerGameMech.hasGun = true;
-                }
+                    SelectGun();    
+        }
                 else
                 {
                     PlayerGameMech.hasGun = false;
                 }
+    }
+
+    void SelectGun()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            selectedWeaponIndex = 0;
+            weapons[0].gameObject.SetActive(true);
+            weapons[1].gameObject.SetActive(false);
+            weapons[2].gameObject.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            selectedWeaponIndex = 1;
+            weapons[0].gameObject.SetActive(false);
+            weapons[1].gameObject.SetActive(true);
+            weapons[2].gameObject.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            selectedWeaponIndex = 2;
+            weapons[0].gameObject.SetActive(false);
+            weapons[1].gameObject.SetActive(false);
+            weapons[2].gameObject.SetActive(true);
+        }
+        else
+        {
+            selectedWeaponIndex = 0;
+            weapons[0].gameObject.SetActive(true);
+            weapons[1].gameObject.SetActive(false);
+            weapons[2].gameObject.SetActive(false);
+        }
     }
 }
